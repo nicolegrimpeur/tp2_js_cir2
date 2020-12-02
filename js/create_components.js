59b9d1d0bc31aceb5077ad161c86e87e66a39478
flolamgit <florian59226@hotmail.fr>
2020-12-02 12:08:04 +0100

    Update create_components.js

--- js/create_components.js
+++ js/create_components.js
@@ -1,7 +1,41 @@
 let lanceComponent = function () {
-
+    if (verifieContent('components')) {
+        createComponents();
+    }
+    else {
+        let currentDiv = document.getElementById('components');
+        let child;
+        while (!verifieContent('components')) {
+            child = currentDiv.lastElementChild;
+            currentDiv.removeChild(child);
+        }
+        createComponents();
+    }
 }
 
 let createComponents = function () {
+    let affichage;
+    for (let array of components_data) {
+        // Nom - Type - Composants - Prix - Achetable en vrac - Utilisation du produit (en %) - Besoin d'un réipient
+        affichage = [array["name"], array["type"], array["price"]];
 
+        // affichage = [array["buyInBulk"], array["percentUsed"], array["needRecipe"]];
+        calculVPR(array["buyInBulk"], array["percentUsed"], array["needRecipe"]).forEach(value => affichage.push(value));
+
+        addList(affichage, 'components');
+    }
+}
+
+let calculVPR = function (array) {
+    let vrac = true;
+    let percent;
+    let recipe = true;
+
+    for (let float of array) {
+        if (!Number(components_data[float]["buyInBulk"]) && vrac) vrac = false;
+        percent = Number(components_data[float]["percentUsed"]) * 100);
+        if (!Number(components_data[float]["needRecipe"]) && recipe) recipe = false;
+    }
+
+    return [(vrac ? "Oui" : "Non"), percent, (recipe ? "Oui" : "Non")];
 }
