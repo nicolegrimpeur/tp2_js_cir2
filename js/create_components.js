a7ecfc728a65a448a9b75432875afe6e6846433f
flolamgit <florian59226@hotmail.fr>
2020-12-05 20:26:05 +0100

    Update create_components.js

--- js/create_components.js
+++ js/create_components.js
@@ -18,24 +18,8 @@ let createComponents = function () {
     for (let array of components_data) {
         // Nom - Type - Composants - Prix - Achetable en vrac - Utilisation du produit (en %) - Besoin d'un réipient
         affichage = [array["name"], array["type"], array["price"]];
-
-        // affichage = [array["buyInBulk"], array["percentUsed"], array["needRecipe"]];
-        calculVPR(array["buyInBulk"], array["percentUsed"], array["needRecipe"]).forEach(value => affichage.push(value));
+        affichage.push(Number(array["buyInBulk"]) ? "Oui" : "Non", String(Number(array["percentUsed"]) * 100), Number(array["needRecipe"]) ? "Oui" : "Non");
 
         addList(affichage, 'components');
     }
-}
-
-let calculVPR = function (array) {
-    let vrac = true;
-    let percent;
-    let recipe = true;
-
-    for (let float of array) {
-        if (!Number(components_data[float]["buyInBulk"]) && vrac) vrac = false;
-        percent = Number(components_data[float]["percentUsed"]) * 100);
-        if (!Number(components_data[float]["needRecipe"]) && recipe) recipe = false;
-    }
-
-    return [(vrac ? "Oui" : "Non"), percent, (recipe ? "Oui" : "Non")];
 }
